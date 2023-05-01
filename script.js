c333314f6b3a48adba0f3c2196d741aba434c0c7
alinvarga95 <118473146+alinvarga95@users.noreply.github.com>
2023-05-01 13:22:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function hideSlide() {
   const items = [
     {
         name: 'Cat Food',
-        image: 'https://images.unsplash.com/photo-1588740841784-53ca3692020a',
+        image: 'https://cdn11.bigcommerce.com/s-lmdkge2gql/images/stencil/1280x1280/products/4350/5129/Whiskas%20Complete%20Chicken__69188.1651792692.jpg?c=1',
         price: 10.99,
         description: 'Premium quality cat food for your feline friend.'
     },
     {
         name: 'Dog Toy',
-        image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba',
+        image: 'https://cdn.shopify.com/s/files/1/0582/2047/8653/products/gipet6029a_1_1800x1800.jpg?v=1669983332',
         price: 5.99,
         description: 'A fun and durable toy for your dog to play with.'
     },
     {
         name: 'Fish Tank',
-        image: 'https://images.unsplash.com/photo-1607526875686-d6aaef98a7f1',
+        image: 'https://m.media-amazon.com/images/I/51ZIxpmdMfL._AC_.jpg',
         price: 50.00,
         description: 'A beautiful and spacious fish tank for your aquatic pets.'
     },
@@ -29,10 +29,10 @@ items.forEach(item => {
     const div = document.createElement('div');
     div.classList.add('item');
     div.innerHTML = `
-        <img src="${item.image}" alt="${item.name}">
-        <h2>${item.name}</h2>
-        <p>${item.description}</p>
-        <p>$${item.price.toFixed(2)}</p>
+        <img src="£{item.image}" alt="£{item.name}">
+        <h2>£{item.name}</h2>
+        <p>£{item.description}</p>
+        <p>££{item.price.toFixed(2)}</p>
         <button>Add to Cart</button>
     `;
     main.appendChild(div);
